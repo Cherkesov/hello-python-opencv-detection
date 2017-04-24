1f7221fc6f63487e6f7f8da73ce8853626cf1acc
GoForBroke <go.for.broke1006@gmail.com>
2017-04-25 02:40:57 +0300

    Init commit

--- app.py
+++ app.py
@@ -2,46 +2,29 @@ import numpy as np
 import cv2
 
 # load the games image
-image = cv2.imread("images/face-9.jpg")
+image = cv2.imread("images/face-6.jpg")
 
-# find the red color game in the image
 # upper = np.array([65, 65, 255])
 # lower = np.array([0, 0, 200])
+# mask = cv2.inRange(image, lower, upper)
+# (contours, _) = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# (contours, _) = cv2.findContours(mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-# upper = np.array([32, 32, 255])
-# lower = np.array([0, 0, 128])
+imgray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-# upper = np.array([200, 120, 110])
-# lower = np.array([150, 70, 60])
-
-# upper = np.array([200, 180, 255])
-# lower = np.array([150, 70, 60])
-
-# Select dark areas
-# upper = np.array([64, 64, 64])
-# lower = np.array([0, 0, 0])
-
-#
-upper = np.array([240, 120, 120])
-lower = np.array([64, 64, 64])
-
-mask = cv2.inRange(image, lower, upper)
-
-# find contours in the masked image and keep the largest one
-(cnts, _) = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-if len(cnts) == 0:
+if len(contours) == 0:
     print "Expected color not found!"
     exit(0)
 
-print cnts
-c = max(cnts, key=cv2.contourArea)
+print contours
+c = max(contours, key=cv2.contourArea)
 
-# approximate the contour
-peri = cv2.arcLength(c, True)
-approx = cv2.approxPolyDP(c, 0.05 * peri, True)
-
-# draw a green bounding box surrounding the red game
+# peri = cv2.arcLength(c, True)
+# approx = cv2.approxPolyDP(c, 0.05 * peri, True)
 # cv2.drawContours(image, [approx], -1, (0, 255, 0), 4)
-cv2.drawContours(image, cnts, -1, (0, 255, 0), 4)
+
+cv2.drawContours(image, contours, -1, (0, 255, 0), 4)
 cv2.imshow("Image", image)
 cv2.waitKey(0)
